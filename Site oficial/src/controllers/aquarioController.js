64a644b2f69ec2aa24330dc773755b634ceb045b
Davissantan4 <davi.ssilva@sptech.school>
2025-05-15 23:33:17 -0300

    Correção da integração com a API web-data-viz

--- Site oficial/src/controllers/aquarioController.js
+++ Site oficial/src/controllers/aquarioController.js
@@ -1,9 +1,9 @@
-var aquarioModel = require("../models/plataformaModel");
+var aquarioModel = require("../models/aquarioModel");
 
-function buscarPlataformasPorEmpresa(req, res) {
+function buscarAquariosPorEmpresa(req, res) {
   var idUsuario = req.params.idUsuario;
 
-  aquarioModel.buscarPlataformasPorEmpresa(idUsuario).then((resultado) => {
+  aquarioModel.buscarAquariosPorEmpresa(idUsuario).then((resultado) => {
     if (resultado.length > 0) {
       res.status(200).json(resultado);
     } else {
@@ -44,6 +44,6 @@ function cadastrar(req, res) {
 }
 
 module.exports = {
-  buscarPlataformasPorEmpresa,
+  buscarAquariosPorEmpresa,
   cadastrar
 }
--- Site oficial/src/controllers/usuarioController.js
+++ Site oficial/src/controllers/usuarioController.js
@@ -20,7 +20,7 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarPlataformasPorEmpresa(resultadoAutenticar[0].idEmpresa)
+                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
                             .then((resultadoAquarios) => {
                                 if (resultadoAquarios.length > 0) {
                                     res.json({
